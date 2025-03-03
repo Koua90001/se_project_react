118d1fb63b299434080697694cc4c2f892e2cb80
Koua90001 <oumarkouakou@yahoo.com>
2025-03-03 01:31:51 -0500

    1st correction

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,20 +1,22 @@
 import "./ItemModal.css";
 
-function ItemModal({activeModal, onClose, card}) {
-return <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
-    <div className="modal__content  modal__content_type_image">
-    <button 
-        onClick={onClose}
-        type="button" 
-        className="item-modal__close">
-        </button>
-        <img src={card.link} alt="" className="item-modal__image" />
+function ItemModal({ activeModal, onClose, card }) {
+  return (
+    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+      <div className="modal__content  modal__content_type_image">
+        <button
+          onClick={onClose}
+          type="button"
+          className="item-modal__close"
+        ></button>
+        <img src={card.link} alt="Modal__image" className="item-modal__image" />
         <div className="item-modal__footer">
-            <h2 className="item-modal__caption">{card.name}</h2>
-            <p className="item-modal__weather">Weather:{card.weather}</p>
+          <h2 className="item-modal__caption">{card.name}</h2>
+          <p className="item-modal__weather">Weather:{card.weather}</p>
         </div>
+      </div>
     </div>
-</div>
+  );
 }
 
 export default ItemModal;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,32 +3,32 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
 
-function Main({weatherData, handleCardClick}) {
-
-    return (
+function Main({ weatherData, handleCardClick }) {
+  return (
     <main className="main">
       <WeatherCard weatherData={weatherData} />
-        <p className="cards__text">
-          Today is {weatherData.temp.F} &deg; F / You may want to wear:
-        </p>
+      <p className="cards__text">
+        Today is {weatherData.temp.F} &deg; F / You may want to wear:
+      </p>
       <section className="cards">
         <ul className="cards__list">
-        {defaultClothingItems
-           .filter((item) => {
-             return item.weather === weatherData.type;
-        })
-        .map((item) => {
-    
-            return <ItemCard 
-            key={item._id} 
-            item={item} 
-            onCardClick={handleCardClick} 
-            />;
-        })}
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={handleCardClick}
+                />
+              );
+            })}
         </ul>
       </section>
     </main>
-    );
+  );
 }
 
 export default Main;
